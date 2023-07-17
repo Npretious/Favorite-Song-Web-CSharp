50a45142f3e0838dff99c85b0c437f069ff55a5d
Nick Pretious <129784159+Npretious@users.noreply.github.com>
2023-07-16 23:09:51 -0600

    Add project files.

---
+++ wwwroot/js/site.js
@@ -0,0 +1,8 @@
+﻿// site.js //
+function newColor() {
+    document.getElementById("Fave_Song").style.color = "black";
+}
+
+function oldColor() {
+    document.getElementById("Fave_Song").style.color = "red";
+}
